d7bbd4b7f271bc0c7c7e53581c646f4b59ea6f86
saroj kandel <saroj.kandel5856@gmail.com>
2026-01-17 22:53:43 +0545

    req_handler

--- DEV_TINDER07/SRC/app.js
+++ DEV_TINDER07/SRC/app.js
@@ -3,13 +3,41 @@ const express = require('express')
 const app = express()
 
 // request handler !!!
+
+app.use("/admin" , (req , res , next)=>{
+    // res.send("hello from the admin pannel")
+    console.log("hello from 1st request handler!!!!");
+    next();
+} , (request , response , next)=>{
+  response.send("this is the respoonse from 2nd req handler")
+}
+)
+
+app.get( "/saroj", (req, res ,next)=>{
+   console.log("hello from 1s handler");
+   next()
+} , (req, res)=>{
+    res.send("respond granted from 2nd")
+
+})
+  
+app.get("/user/:userid/:name/:age" , (req , res)=>{
+   console.log(req.query)
+   console.log(req.params)
+    res.send({name:"saroj" , age:20})
+})
+app.get("/*fly $/" , (req , res)=>{
+   console.log(req.query)
+   console.log(req.params)
+    res.send({name:"saroj" , age:20})
+})
+
+
 app.use("/" , ( req , res)=>{
+    console.log(req.query)
     res.send("hello from the dashboard!!!!")
 })
 
-app.use("/admin" , (req , res)=>{
-    res.send("hello from the admin pannel")
-})
 
 
 app.listen(3000 , ()=>{
